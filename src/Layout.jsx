cbd31db13b9ae673034f660788d89869c6374f8a
Britt Emanuel <britt.emanuel@student.hogent.be>
2026-02-08 13:45:24 +0100

    soort start animatie

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx';
+import IntroLoader from './components/Hero/IntroLoader.jsx';
 
 const Layout = () => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div style={{ display: 'flex' }}>
+      {!loaded && <IntroLoader onComplete={() => setLoaded(true)} />}
       <Navbar />
       <div className='main-content'>
         <Outlet />
