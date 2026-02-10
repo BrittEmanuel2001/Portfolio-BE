d3d9ea18e34fbf0ad67cfa09f8627d3ac047b5fc
Britt Emanuel <britt.emanuel@student.hogent.be>
2026-02-10 15:03:31 +0100

    wip

---
+++ src/api/ProjectsData.js
@@ -0,0 +1,20 @@
+const category = [ "Development", "Design"]
+
+const projects = [
+    {
+        id: 1,
+        name: "Project",
+        image: "Project_x",
+        category: category[0],
+        description: "Description",
+        tools: [
+            { icon: "cibJava", name: "Java", type: "language" },
+            { icon: "cibC", name: "C#", type: "language" }
+        ],
+        team: [
+            { name: "Alex Emanuel", link: "https://www.linkedin.com/in/alex-emanuel/" }
+        ]
+    }
+];
+
+export default projects;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,15 +5,36 @@ const Projects = () => {
     return (
         <div className="projects-section white-border">
             <motion.div
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: false, amount: 0.1 }}
-            transition={{ duration: 1, ease: "easeOut" }}
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: false, amount: 0.1 }}
+                transition={{ duration: 1, ease: "easeOut" }}
             >
                 <div className="outside-container">
                     <div className="inside-container">
                         <h3 id="werk">Werk</h3>
                         <h2>Projectoverzicht</h2>
+
+                        <div className="images-container">
+                            <div className="project-card">
+                                <img className="project-image" src="./projects/Project_CampusApp.jpg" />
+                            </div>
+                            <div className="project-card">
+                                <img className="project-image" src="./projects/Project_CampusApp.jpg" />
+                            </div>
+                            <div className="project-card">
+                                <img className="project-image" src="./projects/Project_CampusApp.jpg" />
+                            </div>
+                            <div className="project-card">
+                                <img className="project-image" src="./projects/Project_CampusApp.jpg" />
+                            </div>
+                            <div className="project-card">
+                                <img className="project-image" src="./projects/Project_CampusApp.jpg" />
+                            </div>
+                            <div className="project-card">
+                                <img className="project-image" src="./projects/Project_CampusApp.jpg" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </motion.div>
